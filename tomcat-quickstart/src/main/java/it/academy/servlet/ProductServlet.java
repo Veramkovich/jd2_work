02db60d8ba447639b7d0a113800b05dd60d6858c
Aliaksandr Veramkovich <averamkovich@allot.com>
2020-12-29 10:30:56 +0300

    Lesson 8: DAO, DTO, liquibase

--- tomcat-quickstart/src/main/java/it/academy/servlet/ProductServlet.java
+++ tomcat-quickstart/src/main/java/it/academy/servlet/ProductServlet.java
@@ -1,49 +1,55 @@
 package it.academy.servlet;
 
+import it.academy.data.DaoFactory;
+import it.academy.data.DatabaseName;
+import it.academy.data.ProductSpec;
+import it.academy.data.ProductSpecDao;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.*;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet(name = "ProductServlet", urlPatterns = "/products")
 public class ProductServlet extends HttpServlet {
 
+    DaoFactory daoFactory;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        final String databaseName = config.getServletContext().getInitParameter("database.name");
+        try {
+            daoFactory = DaoFactory.getInstance(DatabaseName.valueOf(databaseName));
+        } catch (ClassNotFoundException e) {
+            throw new ServletException(e);
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            PrintWriter writer = resp.getWriter();
 
-            final Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/product?serverTimezone=UTC",
-                    "root",
-                    "root"
-            );
-
-            final Statement statement = connection.createStatement();
-
-            final ResultSet resultSet = statement.executeQuery("select ps.id, \n" +
-                    "    ps.product_name, \n" +
-                    "    psp.product_spec_price\n" +
-                    "from product_spec as ps, product_spec_price as psp\n" +
-                    "where ps.id = psp.product_spec_id;");
-
-            final PrintWriter writer = resp.getWriter();
-            while (resultSet.next()) {
-                int id = resultSet.getInt(1);
-                String name = resultSet.getString(2);
-                float price = resultSet.getFloat(3);
-
-                System.out.println("id=" + id + " name=" + name + " price=" + price);
-                writer.println("id=" + id + " name=" + name + " price=" + price);
+            ProductSpecDao productSpecDao = daoFactory.getProductSpecDao();
+            final List<ProductSpec> productSpecs = productSpecDao.readAll();
+            for (ProductSpec product : productSpecs) {
+                System.out.println("id=" + product.getId() +
+                        " name=" + product.getProductName() +
+                        " details=" + product.getProductDetails());
+                writer.println("id=" + product.getId() +
+                        " name=" + product.getProductName() +
+                        " details=" + product.getProductDetails() +
+                        " date=" + product.getProductDate());
             }
 
-            resultSet.close();
-            statement.close();
-            connection.close();
-        } catch (ClassNotFoundException | SQLException | IOException e) {
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
     }
